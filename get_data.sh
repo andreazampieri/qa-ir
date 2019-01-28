33f97fa968b3c0a6c2357b2738f2ed0561bbeb90
andreazampieri <andreazampieri95@gmail.com>
2019-01-28 17:43:26 +0100

    fix get_data.sh for colab

--- get_data.sh
+++ get_data.sh
@@ -1,6 +1,7 @@
 mkdir qa-ir/data
 mkdir qa-ir/data/raw
-cd qa-ir/original_data && wget http://cs.jhu.edu/~xuchen/packages/jacana-qa-naacl2013-data-results.tar.bz2 && tar xvjf jacana-qa-naacl2013-data-results.tar.bz2 && unzip WikiQACorpus.zip && cd ..
+cd qa-ir/original_data && wget http://cs.jhu.edu/~xuchen/packages/jacana-qa-naacl2013-data-results.tar.bz2 && tar xvjf jacana-qa-naacl2013-data-results.tar.bz2 && unzip WikiQACorpus.zip && cd ..;
+pwd
 mv original_data/jacana-qa-naacl2013-data-results /data/raw/trecqa
 mv original_data/WikiQACorpus data/raw/wikiqa
 
